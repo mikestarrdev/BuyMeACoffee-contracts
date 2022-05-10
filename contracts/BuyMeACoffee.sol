f458752e22709fa95f824c5109006235648109d0
Mike Starr <imhs@protonmail.com>
2022-05-10 11:38:47 -0700

    Deployed to Goerli test network

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+// Deployed to Goerli at 0x310e8362A1f405BD89B8a7471471CA07e5730f9e
+
 contract BuyMeACoffee {
     // Event to emit when a Memo is created
     event NewMemo(
@@ -24,7 +26,7 @@ contract BuyMeACoffee {
     address payable owner;
 
     constructor() {
-        owner = payable msg.sender;
+        owner = payable(msg.sender);
     }
 
     /*
@@ -32,7 +34,7 @@ contract BuyMeACoffee {
     * @param _name name of coffee buyer
     * @param _message a nice message for the coffee buyer
     */
-    function buyCoffee(string memory _name, string memory _message) payable {
+    function buyCoffee(string memory _name, string memory _message) public payable {
         require(msg.value > 0, "Can't buy coffee with 0 eth");
 
         // add the memo to storage
--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -1,30 +1,67 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+// returns the Ether balance of a given address
 async function getBalance(address) {
   const balanceBigInt = await hre.waffle.provider.getBalance(address);
   return hre.ethers.utils.formatEther(balanceBigInt);
 }
 
+// Logs the Ether balances for a list of addresses
+async function printBalances(addresses) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx ++;
+  }
+}
+
+// Logs the memos stored on-chain from coffee purchases
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  // Get example accounts
+  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+  
+  // Get the contract deploy & deploy
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+  await buyMeACoffee.deployed();
+  console.log("BuyMeACoffee deployed to ", buyMeACoffee.address);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // Check balances before the offee purchase
+  const addresses = [owner.address, tipper.address, buyMeACoffee.address]
+  console.log("== start ==");
+  await printBalances(addresses)
 
-  await greeter.deployed();
+  // Buy the owner a few coffees
+  const tip = { value: hre.ethers.utils.parseEther("1") };
+  await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "You're the best!", tip);
+  await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Amazing Teacher :)", tip);
+  await buyMeACoffee.connect(tipper3).buyCoffee("Kay", "I love my PoK NFT", tip);
 
-  console.log("Greeter deployed to:", greeter.address);
+  // check balances after coffee purchase
+  console.log("== bought coffee ==");
+  await printBalances(addresses);
+
+  // withdraw funds
+  await buyMeACoffee.connect(owner).withdrawTips();
+
+  // check balances after withdraw
+  console.log("== withdrawTips ==");
+  await printBalances(addresses);
+
+  // Read all the memos left for the owner
+  console.log("== memos ==");
+  const memos = await buyMeACoffee.getMemos();
+  printMemos(memos);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
